e708847cfd2ae7fe4e2a0c7fcd646ae988e07fbb
Dongri Jin <dongrium@gmail.com>
2024-10-23 14:45:13 +0900

    auto commit

--- install.sh
+++ install.sh
@@ -1,59 +1,58 @@
 #!/bin/bash
+set -e
 
-# Function to display the main menu
 show_main_menu() {
   echo "Please choose a programming language to install:"
-  echo "1) Go"
-  echo "2) Rust"
+  echo "1) Rust"
+  echo "2) Go"
   echo "3) Node.js"
-  echo "4) Ruby"
-  echo "5) Python"
-  echo -n "Enter the number corresponding to your choice: "
+  printf "Enter the number: "
 }
 
-# Function to display continuation menu
 show_continue_menu() {
   echo ""
-  echo "What would you like to do?"
+  echo "Do you want to continue or exit?"
   echo "0) Return to main menu"
   echo "1) Exit"
-  echo "Enter your choice: "
+  printf "Enter your choice: "
 }
 
-# Function to display installation steps for Go
-install_go() {
-  echo "Installing Go..."
-  echo "1. Download Go: https://golang.org/dl/"
-  echo "2. Install it: sudo tar -C /usr/local -xzf go<version>.tar.gz"
-  echo "3. Add Go to your PATH: export PATH=\$PATH:/usr/local/go/bin"
-}
-
-# Function to display installation steps for Rust
 install_rust() {
   echo "Installing Rust..."
-  echo "1. Run the following command: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
-  echo "2. Follow the on-screen instructions."
+  echo ""
+  echo "https://www.rust-lang.org/tools/install"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "$ url --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+  echo "$ source \$HOME/.cargo/env"
+  echo "$ rustc --version"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 }
 
-# Function to display installation steps for Node.js
+install_go() {
+  echo "Installing Go..."
+  echo ""
+  echo "https://golang.org/dl/"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "$ wget https://go.dev/dl/go<version>.darwin-amd64.tar.gz"
+  echo "$ sudo tar -C /usr/local -xzf go<version>.darwin-amd64.tar.gz"
+  echo "$ vim ~/.zshrc"
+  echo "export PATH=\$PATH:/usr/local/go/bin"
+  echo "$ source ~/.zshrc"
+  echo "$ go version"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+}
+
 install_node() {
   echo "Installing Node.js..."
-  echo "1. Download and install Node.js: https://nodejs.org"
-  echo "2. Verify installation: node -v"
-}
-
-# Function to display installation steps for Ruby
-install_ruby() {
-  echo "Installing Ruby..."
-  echo "1. Install Ruby using a package manager: sudo apt install ruby-full (Ubuntu)"
-  echo "2. Verify installation: ruby -v"
-}
-
-# Function to display installation steps for Python
-install_python() {
-  echo "Installing Python..."
-  echo "1. Install Python using a package manager: sudo apt install python3 (Ubuntu)"
-  echo "2. Verify installation: python3 --version"
+  echo ""
+  echo "https://github.com/nvm-sh/nvm"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo "$ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/<version>/install.sh | bash"
+  echo "$ source ~/.zshrc"
+  echo "$ nvm ls-remote"
+  echo "$ nvm install <version>"
+  echo "$ node --version"
+  echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 }
 
 # Main loop
@@ -61,21 +60,16 @@ while true; do
   show_main_menu
   read choice < /dev/tty
 
+  echo ""
   case $choice in
     1)
-      install_go
+      install_rust
       ;;
     2)
-      install_rust
+      install_go
       ;;
     3)
       install_node
-      ;;
-    4)
-      install_ruby
-      ;;
-    5)
-      install_python
       ;;
     *)
       echo "Invalid choice. Please try again."
